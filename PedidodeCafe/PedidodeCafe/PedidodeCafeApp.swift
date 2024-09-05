ae16387932cb00dfd7fc6a1a95ac35d50c660d13
Erasmojf <erasmogohiper@gmail.com>
2024-09-05 14:45:21 -0300

    feat: my first commit creat list orders

---
+++ PedidodeCafe/PedidodeCafe/PedidodeCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedidodeCafeApp.swift
+//  PedidodeCafe
+//
+//  Created by Erasmo  on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PedidodeCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
